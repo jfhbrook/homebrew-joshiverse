b61eaabc45933dc54b1f55ac54fe6edfcf97dc7a
Josh Holbrook <josh.holbrook@gmail.com>
2024-05-02 19:41:28 -0800

    cronkite depends on node

--- Formula/cronkite.rb
+++ Formula/cronkite.rb
@@ -7,7 +7,7 @@ class Cronkite < Formula
   sha256 "7422ed2590b55da14dc21c6107506af5bb9df1bea631ced94c6d39a851c52a41"
   license "Apache-2.0"
 
-  depends_on ""
+  depends_on "node"
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
