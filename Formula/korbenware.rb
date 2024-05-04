377f1f260e2a3c4b42860b7e0490ce3632efd768
Josh Holbrook <josh.holbrook@gmail.com>
2024-05-04 14:27:41 -0800

    korbenware v1.0.0-1

--- Formula/korbenware.rb
+++ Formula/korbenware.rb
@@ -1,9 +1,15 @@
 class Korbenware < Formula
   desc "linux/unix desktop cli software"
   homepage "https://github.com/jfhbrook/public/tree/main/korbenware"
-  url "https://github.com/jfhbrook/public/releases/download/korbenware-0.7.0-1/korbenware-0.7.0.tar.gz"
-  sha256 "cd7a34d564f4fdf6a3fd01161e6a38987f835a4bdb8bcb22b61f0e91d6618ad8"
+  url "https://github.com/jfhbrook/public/releases/download/korbenware-1.0.0-1/korbenware-1.0.0.tar.gz"
+  sha256 "f4e2933bba7e8256d89296b1aeac226c1b603de02f553548f80285c995639f68"
   license "MPL-2.0"
+
+  depends_on "bat" => :recommended
+  depends_on "fzf"
+  depends_on "poppler" => :recommended
+  depends_on "python"
+  depends_on "viu"
 
   def install
     bin.install Dir["bin/*"]
