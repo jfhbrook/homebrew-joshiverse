5432cfbe312332c38bda4d64b8041e0f3420702c
Josh Holbrook <josh.holbrook@gmail.com>
2025-03-23 12:42:40 -0800

    korbenware v1.0.2-1

--- Formula/korbenware.rb
+++ Formula/korbenware.rb
@@ -1,8 +1,8 @@
 class Korbenware < Formula
   desc "linux/unix desktop cli software"
   homepage "https://github.com/jfhbrook/public/tree/main/korbenware"
-  url "https://github.com/jfhbrook/public/releases/download/korbenware-1.0.1-1/korbenware-1.0.1.tar.gz"
-  sha256 "481eac21be51397159e76d5033308bceb4b374a346affbe0f4195af7c4b3b753"
+  url "https://github.com/jfhbrook/public/releases/download/korbenware-1.0.2-1/korbenware-1.0.2.tar.gz"
+  sha256 "698115229b99bd9422137a079517a2de476091d9c4913155a67c3a11ac10a955"
   license "MPL-2.0"
 
   depends_on "bat" => :recommended
